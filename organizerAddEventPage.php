785dc2ec263fe678f80538d6bb6ddfa7a911af34
raadalnofaly <107355997+raadalharthi@users.noreply.github.com>
2024-04-19 19:56:28 +0300

    ;l

--- organizerAddEventPage.php
+++ organizerAddEventPage.php
@@ -9,15 +9,99 @@
     if (empty($_SESSION['organizerID'])) {
         require_once 'include\accessDenied.php';
     } else { ?>
+    
     </head>
 
     <body>
         <?php
         include_once 'include/navigationBar.php';
         ?>
+        <div class="container-fluid ps-md-0">
+            <div class="row g-0">
+                <div class="col-md-8 col-lg-6">
+                    <div class="login d-flex align-items-center py-5">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-9 col-lg-8 mx-auto">
+                                    <h3 class="login-heading mb-4">Add Event</h3>
 
-        <h1>organizerAddEventPage.php</h1>
+                                    <form name="addEvent" action="functions/addEvent.php" onsubmit="return validation()"
+                                        method="POST" enctype="multipart/form-data">
 
+                                        <!-- Image Upload Section -->
+                                        <div class="form-floating mb-3">
+                                            <input type="file" class="form-control" id="image" name="image"
+                                                accept="image/*">
+                                            <label for="image">Upload Image</label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="eventTitle" name="eventTitle"
+                                                placeholder="eventTitle">
+                                            <label for="eventTitle">Event Title<span style="color: red;">
+                                                    *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <select class="form-select" id="eventType" name="eventType">
+                                                <option selected>Select Event Type</option>
+                                                <option value="ACD">Academic Events</option>
+                                                <option value="ART">Art Shows</option>
+                                                <option value="AWC">Award Ceremonies</option>
+                                                <option value="CHE">Charity Events</option>
+                                                <option value="COM">Community Events</option>
+                                                <option value="CFS">Conferences</option>
+                                                <option value="ENT">Entertainment Events</option>
+                                                <option value="EXB">Exhibitions</option>
+                                                <option value="HOL">Holiday Celebrations</option>
+                                                <option value="NET">Networking Events</option>
+                                                <option value="SEM">Seminars</option>
+                                                <option value="SHO">Showcase</option>
+                                                <option value="WRK">Workshops</option>
+                                            </select>
+                                            <label for="eventType">Event Type<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="date" class="form-control" id="eventDate" name="eventDate">
+                                            <label for="eventDate">Event Date<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="time" class="form-control" id="eventTime" name="eventTime">
+                                            <label for="eventTime">Event Time<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="datetime-local" class="form-control" id="registrationDeadline" name="registrationDeadline">
+                                            <label for="registrationDeadline">Registration Deadline<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="eventLocation" name="eventLocation"
+                                                placeholder="Building A11, Main Theater">
+                                            <label for="eventLocation">Event Location<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <textarea class="form-control" id="eventDescription" name="eventDescription"
+                                                placeholder="Anything"></textarea>
+                                            <label for="eventDescription">Event Description<span style="color: red;"> *</span></label>
+                                        </div>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="number" class="form-control" id="eventCapacity" name="eventCapacity"
+                                                placeholder="30" min="1" max="1000">
+                                            <label for="eventCapacity">Event Capacity<span style="color: red;"> *</span></label>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php
 
         include_once 'include/footer.php';
--- organizerMyEventsPage.php
+++ organizerMyEventsPage.php
@@ -16,11 +16,166 @@
         include_once 'include/navigationBar.php';
         ?>
 
-        <h1>organizerMyEventsPage.php</h1>
+        <!-- Page Content -->
+        <div class="container">
+
+            <!-- Page Heading -->
+            <h1 class="my-4">My Events</h1>
+
+            <?php
+            require_once 'include/connection.php';
+
+            $sql = "SELECT * FROM events WHERE organizerID = {$_SESSION['organizerID'][0]}";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo '<div class="row">'; // Start the first row
+        
+                // Counter to track number of cards
+                $counter = 0;
+
+                // Output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    ?>
+                    <div class="col-6 mb-4">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="<?php echo $row['eventImage']; ?>"
+                                alt="<?php echo htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8'); ?>">
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <?php echo htmlspecialchars($row["title"], ENT_QUOTES, 'UTF-8'); ?>
+                                </h4>
+                                <p class="card-text" style="text-align: justify;">
+                                    <?php echo htmlspecialchars($row["description"], ENT_QUOTES, 'UTF-8'); ?>
+                                </p>
+                            </div>
+                            <?php
+                            // Assuming $row is fetched from your database and contains 'date' and 'time' columns
+                            $eventDate = $row['date'];
+                            $eventTime = $row['time'];
+
+                            // Combine the date and time into one string and convert it to a DateTime object
+                            $eventDateTimeString = $eventDate . ' ' . $eventTime;
+                            $eventDateTime = new DateTime($eventDateTimeString);
+                            $now = new DateTime();
+
+                            $attendanceRosterEndDateTime = clone $eventDateTime;
+                            $attendanceRosterEndDateTime->modify('+1 day');
+
+                            // Check if the event date and time has passed compared to the current time
+                            if ($eventDateTime < $now) {
+                                // Event has passed; show the form with download and rating buttons
+                                ?>
+
+                                <form action="organizerEditEventPage.php" method="POST" id="eventForm"
+                                    style="display: flex; justify-content: center; gap: 10px; padding: 0 20px;">
+
+                                    <input type="hidden" value="<?php echo $row['eventID']; ?>" name="eventID" id="eventID" />
+
+                                    <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit"
+                                        value="editEvent" name="action"
+                                        style="flex: 1; background: linear-gradient(to right, #6a11cb, #2575fc); border: none; color: white;">Edit
+                                        Event</button>
+
+                                    <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="button"
+                                        data-bs-toggle="modal" data-bs-target="#deleteEvent"
+                                        style="flex: 1; background: linear-gradient(to right, #06beb6, #48b1bf); border: none; color: white;">Delete
+                                        Event</button>
+                                </form>
+
+                                <div class="modal fade" id="deleteEvent" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalLabel">Delete
+                                                    "<?php echo htmlspecialchars($row["title"], ENT_QUOTES, 'UTF-8'); ?>"</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h5 style="text-align: center;">Are you sure you want to delete this event?</h5>
+                                                <br>
+                                                <form action="functions/deleteEvent.php" method="POST" id="deleteEvent">
+                                                    <input type="hidden" name="eventID" value="<?php echo $row['eventID']; ?>" />
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-primary">Delete Event</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <?php
+                            } else {
+                                // Event has not yet passed; show the disabled button
+                                ?>
+                                <form action="organizerEventStatisticsPage.php" method="POST" id="eventForm"
+                                    style="display: flex; justify-content: center; gap: 10px; padding: 0 20px;">
+
+                                    <input type="hidden" value="<?php echo $row['eventID']; ?>" name="eventID" id="eventID" />
+
+                                    <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit"
+                                        value="eventStatistics" name="action"
+                                        style="flex: 1; background: linear-gradient(to right, #6a11cb, #2575fc); border: none; color: white;">Event
+                                        Statistics</button>
+
+                                    <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="button"
+                                        data-bs-toggle="modal" data-bs-target="#attendanceRoster"
+                                        style="flex: 1; background: linear-gradient(to right, #06beb6, #48b1bf); border: none; color: white;" <?php if ($now < $attendanceRosterEndDateTime) {
+                                            echo "disabled"; } ?>>Attendance
+                                        Roster</button>
+                                </form>
+
+                                <div class="modal fade" id="attendanceRoster" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalLabel">Attendance roster for
+                                                    "<?php echo htmlspecialchars($row["title"], ENT_QUOTES, 'UTF-8'); ?>"</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="functions/attendanceRoster.php" method="POST" id="attendanceRoster">
+                                                    <input type="hidden" name="eventID" value="<?php echo $row['eventID']; ?>" />
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-primary">Update</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <?php
+                    $counter++; // Increment the counter
+        
+                    if ($counter % 2 == 0 && $result->num_rows > $counter) {
+                        echo '</div><div class="row">'; // Close the current row and open a new one if more cards are left to display
+                    }
+                }
+
+                if ($counter % 2 != 0) {
+                    echo '</div>'; // Close the last row if an odd number of cards
+                }
+            } else {
+                echo "0 results";
+            }
+            ?>
+
+        </div>
+        <!-- /.container -->
 
         <?php
-
         include_once 'include/footer.php';
+        $conn->close();
         ?>
     </body>
 
