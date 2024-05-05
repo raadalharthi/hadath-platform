aafd58bc9c35d4fecfff4ec645d3d84543f5fa11
raadalnofaly <107355997+raadalharthi@users.noreply.github.com>
2024-05-05 21:23:34 +0300

    aa

--- include/metaData.php
+++ include/metaData.php
@@ -21,6 +21,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <?php
 session_start();
 
